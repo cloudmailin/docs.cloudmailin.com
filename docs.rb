bc8697d5e1b2c3017c91660a54c003c58d39c746
Steve Smith <github@scsworld.co.uk>
2010-07-13 16:20:27 +0100

    update to support the new file extensions

--- docs.rb
+++ docs.rb
@@ -37,8 +37,8 @@ get '/:id' do
     @title = h1_match[1] if h1_match
     @body = html.gsub(H1_FORMAT, '')
     
-    @docs = Hash[*Dir[File.join(File.dirname(__FILE__), "docs/*.txt")].sort.map do |d|
-      match = d.match(/([^\/]*).txt/)
+    @docs = Hash[*Dir[File.join(File.dirname(__FILE__), "docs/*.md")].sort.map do |d|
+      match = d.match(/([^\/]*).md/)
       if match
         [match[1][0..0].upcase + match[1][1..-1].gsub('_', ' '), match[1]]
       else
@@ -63,5 +63,5 @@ post /\/incoming_mail\/?/i do
 end
 
 def get_path(file_name)
-  File.join(File.dirname(__FILE__), "docs/#{file_name.gsub(/d+_/, '')}.txt")
+  File.join(File.dirname(__FILE__), "docs/#{file_name.gsub(/d+_/, '')}.md")
 end
