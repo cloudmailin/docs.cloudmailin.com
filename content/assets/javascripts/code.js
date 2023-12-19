1a9ae559d1f81fab6a523207d80804c01e62fd40
Steve Smith <scsmith@users.noreply.github.com>
2023-12-19 16:50:32 +0000

    Remove jquery

--- content/assets/javascripts/code.js
+++ content/assets/javascripts/code.js
@@ -1,93 +1,103 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * DS104: Avoid inline assignments
- * DS202: Simplify dynamic range loops
- * DS204: Change includes calls to have a more natural evaluation order
- * DS205: Consider reworking code to avoid use of IIFEs
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const subNames = { 'ruby': 'Rails', 'php': 'PHP', 'javascript': 'Node.js',
-  'c#': '.NET', 'xml': 'XML', 'jsonc': 'JSON', 'json': 'JSON' };
+  'c#': '.NET', 'xml': 'XML', 'jsonc': 'JSON', 'json': 'JSON', 'raw': 'Multipart' };
 
-Array.prototype.unique = function() {
-  let key;
-  let asc, end;
-  const output = {};
-  for (key = 0, end = this.length, asc = 0 <= end; asc ? key < end : key > end; asc ? key++ : key--) { output[this[key]] = this[key]; }
-  return (() => {
-    const result = [];
-    for (key in output) {
-      const value = output[key];
-      result.push(value);
-    }
-    return result;
-  })();
-};
+function groupedCodeBlocks() {
+  let blocks = [];
 
-const groupCodeBlocks = function(codeBlocks, pattern){
-  const groups = [];
-  for (let item of Array.from(codeBlocks)) {
-    if ($(item).prev(pattern).length > 0) {
-      groups[groups.length - 1].push($(item));
-    } else {
-      groups.push([$(item)]);
+  let codeList = document.querySelectorAll('pre code[class^=language-]:not([class="language-"])');
+  let preList = Array.from(codeList).map(code => code.parentElement);
+  // console.log(preList);
+
+  let currentGroup = [];
+  for (let i = 0; i < preList.length; i++) {
+    let pre = preList[i];
+    let nextPre = preList[i + 1];
+    let nextElement = pre.nextElementSibling;
+
+    currentGroup.push(pre);
+
+    // if the next element is the next pre then add to the current group. else start a new one
+    if (nextElement !== nextPre) {
+      // console.log('next element is not next pre');
+      blocks.push(currentGroup);
+      currentGroup = [];
     }
   }
-  return groups;
-};
 
-const showLanguageForGroup = function(languageName, group){
-  let needle;
-  const included = (needle = languageName, Array.from(group.map(item => item.children('code').attr('class'))).includes(needle));
-  const languageSelector = group[0].prev('.language_list');
+  // console.log(blocks);
+  return blocks;
+}
 
-  if (included) {
-    for (let item of Array.from(group)) {
-      if (item.children('code').attr('class') === languageName) {
-        item.show();
+function addLanguageList(block) {
+  // create a new ul containing a list of the lanaguages
+  let ul = document.createElement('ul');
+  let languages = []
+  ul.className = 'languages';
+
+  // for each block, create a new li and add it to the ul
+  block.forEach(pre => {
+    let li = addLanguageSelector(pre, block);
+    if (languages.includes(pre.dataset.language)) {
+      return;
+    }
+    languages.push(pre.dataset.language);
+
+    ul.appendChild(li);
+  });
+
+  let div = document.createElement('div');
+  div.className = 'language_list';
+  div.appendChild(ul);
+
+  // insert the ul before the first pre
+  block[0].parentElement.insertBefore(div, block[0]);
+}
+
+// Add an li, that when clicked will show the selected language in this block
+function addLanguageSelector(pre, block) {
+  let li = document.createElement('li');
+
+  let code = pre.querySelector('code');
+  let language = code.className.replace('language-', '');
+
+  pre.dataset.language = language;
+
+  let subName = subNames[language] || language;
+  let a = document.createElement('a');
+
+  a.href = '#';
+  a.innerHTML = subName;
+  a.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // hide all the pre elements in this block
+    block.forEach(pre => {
+      if (pre.dataset.language === language) {
+        pre.style.display = 'block';
       } else {
-        item.hide();
+        pre.style.display = 'none';
       }
-    }
-
-    languageSelector.find("a[data-language-class]").removeClass('selected');
-    return languageSelector.find(`a[data-language-class='${languageName}']`).addClass('selected');
-  }
-};
-
-const drawLanguageSelector = function(languages, group, groups){
-  const firstItem = group[0];
-  const languageContainer = $("<div class='language_list'><ul class='languages'></ul></div>");
-  for (var language of Array.from(languages)) {
-    let languageName = (language.replace('language-', '').split(' ').map(word => word[0].toUpperCase() + word.slice(1))).join(' ');
-    languageName = subNames[languageName.toLowerCase()] || languageName;
-    const languageClass = language;
-    const languageSelector = $(`<li><a href='javascript:void(0)' data-language-class='${languageClass}'>${languageName}</a></li>`);
-    languageSelector.find('a').click(function(e){
-      language = $(e.target).data('language-class');
-      return Array.from(groups).map((showGroup) =>
-        showLanguageForGroup(language, showGroup));
     });
+  });
 
-    languageContainer.find('ul').append(languageSelector);
-  }
-  firstItem.before(languageContainer);
-  return languageContainer;
-};
+  li.appendChild(a);
 
-jQuery(function($){
-  const pattern = 'pre:has(code[class])';
-  const groups = groupCodeBlocks($(pattern), pattern);
+  return li;
+}
 
-  return (() => {
-    const result = [];
-    for (let group of Array.from(groups)) {
-      const languages = (group.map(codeBlock => $(codeBlock).children('code').attr('class'))).unique();
-      const languageContainer = drawLanguageSelector(languages, group, groups);
-      result.push(showLanguageForGroup(languages[0], group));
-    }
-    return result;
-  })();
+// On page load, find all pre elements that have a code item inside them
+document.addEventListener('DOMContentLoaded', () => {
+  let blocks = groupedCodeBlocks();
+  blocks.forEach(block => {
+    addLanguageList(block);
+
+    let currentLanguage = block[0].dataset.language;
+    block.forEach(pre => {
+      if (pre.dataset.language === currentLanguage) {
+        pre.style.display = 'block';
+      } else {
+        pre.style.display = 'none';
+      }
+    });
+  });
 });
